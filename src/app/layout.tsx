6d0619afd37c9ff3533623fe86c526612c4a3511
Waiz Monazzum <waizmonazzum270@gmail.com>
2025-10-15 23:21:47 +0000

    Ye pwa installable nahi hai ... Shortcut create karne ka option bas arha

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,7 +84,7 @@ export default function RootLayout({
           {`
             if ('serviceWorker' in navigator) {
               window.addEventListener('load', () => {
-                navigator.service-worker.register('/sw.js').then(registration => {
+                navigator.serviceWorker.register('/sw.js').then(registration => {
                   console.log('SW registered: ', registration);
                 }).catch(registrationError => {
                   console.log('SW registration failed: ', registrationError);
